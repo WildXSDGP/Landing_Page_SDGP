34f028325d59b664e91ceccda91318da54ecf1d3
Banuka Malshan <bmagamulla@gmail.com>
2025-11-21 16:15:34 +0530

    setting up all styles done so far

--- src/app/features/features.ts
+++ src/app/features/features.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-features',
-  imports: [CardModule,ButtonModule],
+  imports: [CardModule,ButtonModule,NgFor],
   templateUrl: './features.html',
   styleUrl: './features.css',
 })
 export class Features {
 
   features = [
-  { icon: 'pi pi-map',           title: 'Interactive Map',   description: 'View safari trails & animal zones' },
-  { icon: 'pi pi-cog',           title: 'AI Predictions',    description: 'Suggests best animal viewing times' },
-  { icon: 'pi pi-wifi-off',      title: 'Offline Mode',      description: 'Works even with no signal' },
-  { icon: 'pi pi-exclamation-triangle', title: 'SOS Support', description: 'One-tap emergency alert' },
-  { icon: 'pi pi-camera',        title: 'Wildlife Gallery',  description: 'View & tag safari photos' },
-  { icon: 'pi pi-car',           title: 'Safari Booking',    description: 'Book certified jeep drivers' }
-];
+    { icon: 'pi pi-map', name: 'Interactive Map', description: 'View safari trails & animal zones' },
+    { icon: 'pi pi-cog', name: 'AI Predictions', description: 'Suggests best animal viewing times' },
+    { icon: 'pi pi-wifi-off', name: 'Offline Mode', description: 'Works even with no signal' },
+    { icon: 'pi pi-exclamation-triangle', name: 'SOS Support', description: 'One-tap emergency alert' },
+    { icon: 'pi pi-camera', name: 'Wildlife Gallery', description: 'View & tag safari photos' },
+    { icon: 'pi pi-car', name: 'Safari Booking', description: 'Book certified jeep drivers' }
+  ];
 
 
 }
