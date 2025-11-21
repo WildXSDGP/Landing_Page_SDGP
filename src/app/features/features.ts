9db28b6c8a1cc59e0669593a1d584955b5d48fbf
Banuka Malshan <bmagamulla@gmail.com>
2025-11-21 12:47:18 +0530

    Add features array to Feaures ts file

--- src/app/features/features.ts
+++ src/app/features/features.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Features {
 
-  
+  features = [
+  { icon: 'pi pi-map',           title: 'Interactive Map',   description: 'View safari trails & animal zones' },
+  { icon: 'pi pi-cog',           title: 'AI Predictions',    description: 'Suggests best animal viewing times' },
+  { icon: 'pi pi-wifi-off',      title: 'Offline Mode',      description: 'Works even with no signal' },
+  { icon: 'pi pi-exclamation-triangle', title: 'SOS Support', description: 'One-tap emergency alert' },
+  { icon: 'pi pi-camera',        title: 'Wildlife Gallery',  description: 'View & tag safari photos' },
+  { icon: 'pi pi-car',           title: 'Safari Booking',    description: 'Book certified jeep drivers' }
+];
+
 
 }
