1349b1c7ec74cab4508615a56796d2868a82821d
Banuka Malshan <bmagamulla@gmail.com>
2025-11-21 17:11:32 +0530

    Adding routes to the excisting pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,23 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { TeamPage } from './team-page/team-page';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
-    {path:'',
-        
-    },
+    {path:'',component:App},
+    {path:'Home',redirectTo:'',pathMatch:'full'},
+
+    {
+    path: 'about',
+    children: [
+      { path: '', redirectTo: 'team', pathMatch: 'full' }, // default
+      { path: 'team', component: TeamPage },
+      { path: 'wildx', component: PageNotFound },
+    ]
+  },
+  { path: 'parks', component: PageNotFound },
+  { path: 'map', component: PageNotFound },
+  { path: 'features', component: PageNotFound },
+  { path: 'about-us', component: TeamPage},
+  { path: '**', component: PageNotFound},
 ];
