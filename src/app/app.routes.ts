7879f53e9613b38eaa4ff89f3592755a9be6007e
Banuka Malshan <bmagamulla@gmail.com>
2025-12-02 10:41:30 +0530

    App features page move to the separate page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { TeamPage } from './team-page/team-page';
 import { PageNotFound } from './page-not-found/page-not-found';
 import { Landing } from './pages/landing/landing';
+import { Features } from './features/features';
 
 export const routes: Routes = [
     {path:'',component:Landing},
@@ -18,7 +19,7 @@ export const routes: Routes = [
   },
   { path: 'parks', component: PageNotFound },
   { path: 'map', component: PageNotFound },
-  { path: 'features', component: PageNotFound },
+  { path: 'features', component: Features },
   { path: 'about-us', component: TeamPage},
   { path: '**', component: PageNotFound},
 ];
