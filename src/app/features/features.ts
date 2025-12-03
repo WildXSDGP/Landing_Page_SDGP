431f42debe238432b88e5b08b98648a92d43ef0f
Banuka Malshan <bmagamulla@gmail.com>
2025-12-03 08:01:08 +0530

    improve look and feel of feature page by adding styles

--- src/app/features/features.ts
+++ src/app/features/features.ts
@@ -13,8 +13,8 @@ export class Features {
 
   features = [
     { icon: 'pi pi-map', name: 'Interactive Map', description: 'View safari trails & animal zones' },
-    { icon: 'pi pi-cog', name: 'AI Predictions', description: 'Suggests best animal viewing times' },
-    { icon: 'pi pi-wifi-off', name: 'Offline Mode', description: 'Works even with no signal' },
+    { icon: 'pi pi-warehouse', name: 'Accommadation Details', description: 'Best accommodation nearby national park' },
+    { icon: 'pi pi-sort-alt-slash', name: 'Offline Mode', description: 'Works even with no signal' },
     { icon: 'pi pi-exclamation-triangle', name: 'SOS Support', description: 'One-tap emergency alert' },
     { icon: 'pi pi-camera', name: 'Wildlife Gallery', description: 'View & tag safari photos' },
     { icon: 'pi pi-car', name: 'Safari Booking', description: 'Book certified jeep drivers' }
